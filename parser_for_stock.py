8a1a01604c06926935d0cbdcde4a3acba7ff284f
Vincent Hsu <ab89414@gmail.com>
2021-11-05 08:27:13 +0800

    add dataBase class

--- parser_for_stock.py
+++ parser_for_stock.py
@@ -6,13 +6,33 @@ import json
 sourceUrl = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp"
 monitorStock = ['2330', '2454', '0050', '0056', '00878']
 
-def get_data_from_server():
+class dataBase:
+    def __init__(self)
+        self.dataDict = dict()
+        self.timeList = list()
+
+
+def get_data_from_server(stockData):   #dataBase should be a dictionary
+    
     data = {'ex_ch': '|'.join(['tse_{}.tw'.format(stockInd) for stockInd in monitorStock]), 'json': '1'}
 
     res = requests.get(sourceUrl, params = data)
     res = (res.json())['msgArray']
+    
     for stockInfo in res:
-        print("{0:8.4f}  {1:s}".format(float(stockInfo['z']), stockInfo['n']))
+        stockName = stockInfo['n']
+        stockPrice = stockInfo['z']
+        if (stockName in stockData.dataDict):
+            stockData.dataDict[stockName].append(float(stockPrice))
+        else:
+            stockData.dataDict[stockName] = [float(stockPrice)]
+
+    #for stockInfo in res:
+    #    print("{0:8.4f}  {1:s}".format(float(stockInfo['z']), stockInfo['n']))
+
+
+
+
 
 
 
@@ -20,7 +40,7 @@ def get_data_from_server():
 # other file, so the below code will not be executed
 # The name will be __main__ if this file was exectued directly
 if __name__ == '__main__':    
-    # get_data_from_server()
+    get_data_from_server()
     window = tk.Tk()
     window.title('Stock View')
     window.geometry('800x600')
