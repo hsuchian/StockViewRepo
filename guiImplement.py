f0788b2acca9f6c873aa29eda0bbd406fc8c3701
Hsu <vihsu@mgc.mentorg.com>
2021-11-10 18:57:07 +0800

    code refactor, move the gui related code out of stockInfo

--- guiImplement.py
+++ guiImplement.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
 
 
 class guiMgr:
@@ -14,7 +17,6 @@ class guiMgr:
         buttonFrame = tk.Frame(window, width=w*1/3 , height=h, bg='green')
         #div3 = tk.Frame(window,  width=div_size , height=div_size , bg='blue')
 
-
         plotFrame.grid(row = 0, column = 0,)
         buttonFrame.grid(row = 0, column = 1)
 
@@ -22,6 +24,39 @@ class guiMgr:
         window.rowconfigure(index = 1, weight = 1)
         window.columnconfigure(index = 0, weight = 1)  
         window.columnconfigure(index = 1, weight = 1)
+
+        # class member
         self._window = window
         self._plotFrame = plotFrame
         self._buttonFrame = buttonFrame
+
+    def create_button_by_stock_name_list(self, stockDataBase):
+        for stockName in stockDataBase.get_dataDict():
+            self.create_button(stockName)
+
+    def create_button(self, stockName):
+        tmpButton = tk.Button(self._buttonFrame, 
+                              text = stockName, 
+                              command = lambda : self.plot_cur_data()
+                             )
+        tmpButton.grid()
+
+
+    def plot_cur_data(self, timeList, priceList):
+
+        fig = plt.figure()
+        axis1 = fig.add_subplot(111)
+        axis1.plot(timeList, priceList, '*-b')
+
+        for item in plotTargetFrame.winfo_children():
+            item.destroy()
+
+        canvas = FigureCanvasTkAgg(fig, master=plotTargetFrame)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().grid(row = 0, column = 0)
+        plotTargetFrame.rowconfigure(index = 0, weight = 1)
+        plotTargetFrame.columnconfigure(index = 0, weight =1)
+
+    def create_button(self, buttonTargetFrame, plotTargetFrame):
+        for stockInd in self._dataDict:
+            self._dataDict[stockInd].create_button(buttonTargetFrame, plotTargetFrame)
--- stockClass.py
+++ stockClass.py
@@ -1,7 +1,4 @@
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
 import requests
-import tkinter as tk
 import datetime 
 
 class stockInfo:
@@ -11,12 +8,6 @@ class stockInfo:
         self._stockQueryStr = 'tse_{}.tw'.format(stockInd)
         self._stockInd = stockInd
         self._name = None
-
-    def set_name(self, name):
-        self._name = name
-    
-    def get_name(self):
-        return self._name
 
     def add_price(self, price, time):
         
@@ -36,34 +27,23 @@ class stockInfo:
         self._priceList.append(price)
         self._timeList.append(timeStructTmp)
 
-    def plot_cur_data(self, plotTargetFrame):
+    def get_price_list(self):
+        return self._priceList
 
-        fig = plt.figure()
-        axis1 = fig.add_subplot(111)
-        axis1.plot(self._timeList, self._priceList, '*-b')
+    def get_time_list(self):
+        return self._timeList
 
-        for item in plotTargetFrame.winfo_children():
-            item.destroy()
-
-        canvas = FigureCanvasTkAgg(fig, master=plotTargetFrame)  # A tk.DrawingArea.
-        canvas.draw()
-        canvas.get_tk_widget().grid(row = 0, column = 0)
-        plotTargetFrame.rowconfigure(index = 0, weight = 1)
-        plotTargetFrame.columnconfigure(index = 0, weight =1)
-
-    def create_button(self, buttonTargetFrame, plotTargetFrame):
-        tmpButton = tk.Button(buttonTargetFrame, 
-                              text = self._name, 
-                              command = lambda : self.plot_cur_data(plotTargetFrame),
-                             )
-        tmpButton.grid()
-    
     def get_ind(self):
         return self._stockInd
 
     def get_query_str(self):
         return self._stockQueryStr
 
+    def set_name(self, name):
+        self._name = name
+    
+    def get_name(self):
+        return self._name
 
 class dataBase:
     def __init__(self, sourceUrl):
@@ -87,7 +67,7 @@ class dataBase:
     def get_dataDict(self):
         return self._dataDict
 
-    def get_data_from_server(self, GUIMgr):   #dataBase should be a dictionary
+    def get_data_from_server(self):   #dataBase should be a dictionary
         
         #data = {'ex_ch': '|'.join(['tse_{}.tw'.format(stockInd) for stockInd in monitorStock]), 'json': '1'}
         data = {'ex_ch': '|'.join([self._dataDict[sInd].get_query_str() for sInd in self._dataDict]), 'json': '1'}
@@ -105,13 +85,11 @@ class dataBase:
             #print("{0:8.4f}  {1:s}".format(float(stockInfo['z']), stockInfo['n']))     
         
         print("Update data from server " + sTime)
-        GUIMgr._window.after(5000, self.get_data_from_server, GUIMgr)
-        # update the figure for the current figure
-        self._dataDict[self._curStockInd].plot_cur_data(GUIMgr._plotFrame)
 
 
     def set_name_for_stock(self):
-        data = {'ex_ch': '|'.join([self._dataDict[sInd].get_query_str() for sInd in self._dataDict]), 'json': '1'}
+        data = {'ex_ch': '|'.join([self._dataDict[sInd].get_query_str() 
+                                  for sInd in self._dataDict]), 'json': '1'}
 
         res = requests.get(self._sourceUrl, params = data)
         res = (res.json())['msgArray']
@@ -120,10 +98,6 @@ class dataBase:
             sName = sInfoServer['n']
             sInd = sInfoServer['c']
             self._dataDict[sInd].set_name(sName)
-
-    def create_button(self, buttonTargetFrame, plotTargetFrame):
-        for stockInd in self._dataDict:
-            self._dataDict[stockInd].create_button(buttonTargetFrame, plotTargetFrame)
 
 if __name__ == '__main__':
     print('Cannot execute this file directly')
