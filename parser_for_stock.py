0c3a84457d3a4a72cd2f7ef6ebf3ba956a47a07f
Vincent Hsu <ab89414@gmail.com>
2021-12-02 00:02:26 +0800

    change to pyqt5

--- parser_for_stock.py
+++ parser_for_stock.py
@@ -3,9 +3,10 @@
 
 #import stockClass
 from stockClass import *
-from guiImplement import *
+from guiPYQT5 import *
 import json
 import time
+import sys
 
 
 def start_update_5_sec(GUImgr, TotalStockData):
@@ -33,15 +34,43 @@ if __name__ == '__main__':
     TotalStockData = dataBase(sourceUrl, monitorStock)
     TotalStockData.set_name_for_stock()
 
-    GUIMgr = guiMgr()
-    GUIMgr.init_cur_stock(monitorStock[0])    
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+
 
     # add button, should after the dataBase initialization
     # since the button number and action fcn is decided by 
     # stock info
+    ui.create_button_by_stock_database(TotalStockData)
+
+    fig1 = plt.figure()
+
+    ax1 = fig1.add_subplot(1,2,1)
+    ax1.plot(range(1,6), [11, 10, None, 12, 13 ])
+
+    scene = QtWidgets.QGraphicsScene()
+    # QtWidgets.QGraphicsScene().add
+    scene.addWidget(FigureCanvas(fig1))
+    ui.graphicsView.setScene(scene)
+    ui.graphicsView.show()
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+
+''' old code 
+
+    GUIMgr = guiMgr()
+    GUIMgr.init_cur_stock(monitorStock[0])   
     GUIMgr.create_button_by_stock_name_list(TotalStockData)
 
     
     start_update_5_sec(GUIMgr, TotalStockData)
 
-    GUIMgr._window.mainloop()
+    GUIMgr._window.mainloop() 
+
+'''
--- test_plot.py
+++ test_plot.py
@@ -2,24 +2,16 @@
 
 import matplotlib.pyplot as plt
 
-
+# one figure is one window, one window can contain
+# mutiple sub plot (axis)
 fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,1,1)
+
+ax1 = fig1.add_subplot(1,2,1)
 ax1.plot(range(1,6), [11, 10, None, 12, 13 ])
+
+ax2 = fig1.add_subplot(1,2,2)
+ax2.plot(range(1,6), [11, 10, None, 12, 13 ])
+
+
 plt.show()
-
-a = list()
-a = [ 1,2]
-if a:
-    print("a is not empty")
-
-
-fuck
-hello
-
-
-
-
-test
-fuck1
 
